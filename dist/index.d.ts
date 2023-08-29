30294d39167b1b8949762cd5663b92d502f0d1dd
Sam-Morgan-Tyghe <sam.morgan@hedgehoglab.com>
2023-08-29 22:09:48 +0100

    update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 export { useGetAPI, usePostAPI, usePutAPI, usePatchAPI, useDeleteAPI, } from "./hooks/useEasyHook";
 export { setupGlobalOptions, setupHTTPClient, globalOptions, httpClient, } from "./hooks/setup";
-export { WrapThatApp } from "./hooks/WrapThatApp";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.WrapThatApp = exports.httpClient = exports.globalOptions = exports.setupHTTPClient = exports.setupGlobalOptions = exports.useDeleteAPI = exports.usePatchAPI = exports.usePutAPI = exports.usePostAPI = exports.useGetAPI = void 0;
+exports.httpClient = exports.globalOptions = exports.setupHTTPClient = exports.setupGlobalOptions = exports.useDeleteAPI = exports.usePatchAPI = exports.usePutAPI = exports.usePostAPI = exports.useGetAPI = void 0;
 var useEasyHook_1 = require("./hooks/useEasyHook");
 Object.defineProperty(exports, "useGetAPI", { enumerable: true, get: function () { return useEasyHook_1.useGetAPI; } });
 Object.defineProperty(exports, "usePostAPI", { enumerable: true, get: function () { return useEasyHook_1.usePostAPI; } });
@@ -12,6 +12,4 @@ Object.defineProperty(exports, "setupGlobalOptions", { enumerable: true, get: fu
 Object.defineProperty(exports, "setupHTTPClient", { enumerable: true, get: function () { return setup_1.setupHTTPClient; } });
 Object.defineProperty(exports, "globalOptions", { enumerable: true, get: function () { return setup_1.globalOptions; } });
 Object.defineProperty(exports, "httpClient", { enumerable: true, get: function () { return setup_1.httpClient; } });
-var WrapThatApp_1 = require("./hooks/WrapThatApp");
-Object.defineProperty(exports, "WrapThatApp", { enumerable: true, get: function () { return WrapThatApp_1.WrapThatApp; } });
 //# sourceMappingURL=index.js.map
